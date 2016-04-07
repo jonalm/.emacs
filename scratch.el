e0e1b9597036a14d45d741e8dd6c86f71ef32d50
jonalm <jon.alm.eriksen@gmail.com>
2016-04-07 23:46:22 +0200

    add scratch file

---
+++ scratch.el
@@ -0,0 +1,10 @@
+
+;; Remove dialog boxes which cause emacs to crash on OSX
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
